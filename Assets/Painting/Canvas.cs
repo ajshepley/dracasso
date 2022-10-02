62398d04554c3f617956d711be684abcee69877e
DavidDufour <dadufour@uwaterloo.ca>
2022-10-02 14:26:24 -0400

    Add outro with painting

---
+++ Assets/Painting/Canvas.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvas : MonoBehaviour
+{
+    public Sprite sprite;
+    public float health = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health += 0.1f;
+        if (health >= 110) {
+            health = 1;
+        }
+        var resourceNumber = Mathf.Ceil(health / 10);
+        var resource = "" + resourceNumber;
+        Debug.Log(resource);
+        this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(resource);
+    }
+}
